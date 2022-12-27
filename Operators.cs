fe1308b3c45f439f60e951637d4150476f5288db
Paul Turner <102317855+paulthesoftwareengineer@users.noreply.github.com>
2022-12-26 22:14:55 -0800

    initial

--- Operators.cs
+++ Operators.cs
@@ -11,6 +11,16 @@ namespace OperatorExercise
 
             Console.WriteLine("a / b = {0} with a remainder of {1}", quotient, remainder);
 
+            Console.WriteLine("Enter the radius of the circle: ");
+            var radius = double.Parse(Console.ReadLine());
+            var area = AreaOfCircle(radius);
+            Console.WriteLine($"The area of the circle is: {area}");
+        }
+
+        public static double AreaOfCircle(double r)
+        {
+            const double pi = 3.14159;
+            return pi * Math.Pow(r, 2);
         }
     }
 }
