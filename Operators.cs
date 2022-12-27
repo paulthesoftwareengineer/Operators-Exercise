dcdd5302b4b7ac9024e28b6d17b372718f0e8e04
Paul Turner <102317855+paulthesoftwareengineer@users.noreply.github.com>
2022-12-26 22:10:36 -0800

    initial

---
+++ Operators.cs
@@ -0,0 +1,16 @@
+namespace OperatorExercise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+
+            Console.WriteLine("a / b = {0} with a remainder of {1}", quotient, remainder);
+
+        }
+    }
+}
